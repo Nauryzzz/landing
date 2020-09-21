b1a8349d87da5a33d95d06be0c784792e0fce1e6
Nauryzzz <nauryzzz@inbox.ru>
2020-09-22 01:46:51 +0500

    portfolio js

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -7,4 +7,28 @@
             el.classList.add('active');
         })
     })
+
+    let filers = document.querySelectorAll('[data-link]');
+    
+    filers.forEach(el => {
+        el.addEventListener('click', function(e) {
+            e.preventDefault();
+            
+            let cat = this.dataset.link;
+            let cols = document.querySelectorAll('.portfolio__col');
+            
+            if (cat == "all") {
+                cols.forEach(col => col.classList.remove('hide__category'));                
+            } else {
+                cols.forEach(col => {
+                    if (col.dataset.cat !== cat) {
+                        col.classList.add('hide__category')
+                    } else {
+                        col.classList.remove('hide__category')
+                    }
+                });
+            }
+        });        
+    })
+
 })();
