2d735922173b3e364657860085f8dff0b39f982a
Nauryzzz <nauryzzz@inbox.ru>
2020-10-07 11:30:30 +0500

    Porfolio finish

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,9 +1,9 @@
 (function () {
-    let create__links = document.querySelectorAll('.create__link');
+    let create__links = document.querySelectorAll('.nav__link:not(.nav__link--btn)');
 
     create__links.forEach(el => {
         el.addEventListener('click', () => {
-            document.querySelectorAll('.create__link').forEach(el => el.classList.remove('active'));
+            document.querySelectorAll('.nav__link').forEach(el => el.classList.remove('active'));
             el.classList.add('active');
         })
     })
@@ -31,4 +31,46 @@
         });        
     })
 
+    let btns = document.querySelectorAll('[data-modal]');
+
+    btns.forEach(el => {
+        el.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            let modalId = this.dataset.modal;
+            document.querySelector(modalId).classList.add('show');
+            document.querySelector('body').classList.add('no-scroll');
+        })
+    })
+
+    let modalCloseBtns = document.querySelectorAll('.modal__close');
+    modalCloseBtns.forEach(el => {
+        el.addEventListener('click', function(event) {
+            event.preventDefault();
+            
+            let modalParent = this.closest('.modal');
+            modalParent.classList.remove('show');
+            document.querySelector('body').classList.remove('no-scroll');
+        });
+    });
+
+    let modals = document.querySelectorAll('.modal');
+    modals.forEach(el => {
+        el.addEventListener('click', function (e) {
+            this.classList.remove('show');
+            document.querySelector('body').classList.remove('no-scroll');
+        });
+    });
+
+    let modalDialogs = document.querySelectorAll('.modal__dialog');
+    modalDialogs.forEach(el => {
+        el.addEventListener('click', function (e) {
+            e.stopPropagation();
+        });
+    });
+
 })();
+
+document.addEventListener('DOMContentLoaded', function () {
+
+});
